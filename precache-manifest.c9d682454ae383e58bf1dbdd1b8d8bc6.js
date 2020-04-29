8c5b646ffbea96a68787f856d1a6250829224064
kanumanarmada <kanumanarmada10@gmail.com>
2020-04-29 16:32:37 +0530

    Updates

---
+++ precache-manifest.c9d682454ae383e58bf1dbdd1b8d8bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce382c07abdfd4e432a56fffea8356db",
+    "url": "/narmada-profile-react/index.html"
+  },
+  {
+    "revision": "292b8e0cadf9dd4ecfb3",
+    "url": "/narmada-profile-react/static/css/main.68f21831.chunk.css"
+  },
+  {
+    "revision": "6b299790f34f1c8861ac",
+    "url": "/narmada-profile-react/static/js/2.1c5eba83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/narmada-profile-react/static/js/2.1c5eba83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292b8e0cadf9dd4ecfb3",
+    "url": "/narmada-profile-react/static/js/main.1aef1afd.chunk.js"
+  },
+  {
+    "revision": "ba61c3f447b1d15758db",
+    "url": "/narmada-profile-react/static/js/runtime-main.e28efcad.js"
+  }
+]);
